8816dbc2ce39f14b50cd3f04e4e069690767a171
kambojrahul <rahulkamboj096@gmail.com>
2017-10-08 15:03:33 +0530

    trail commit

---
+++ myDummyApp/src/main/java/myDummyApp/Main.java
@@ -0,0 +1,9 @@
+package myDummyApp;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Hello Maven");
+	}
+}
---
+++ myDummyApp/src/test/java/com/myDummyApp/FirstProg.java
@@ -0,0 +1,13 @@
+package com.myDummyApp;
+
+import org.testng.annotations.Test;
+
+public class FirstProg {
+
+	
+	@Test
+	public void run()
+	{
+		System.out.println("Running through Jenkins");
+	}
+}
